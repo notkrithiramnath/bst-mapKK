8962f50d33ef0d8e663d7765da5da1a84fc24628
notkrithiramnath <krithiramnath@gmail.com>
2025-04-15 00:17:40 -0400

    map shi

--- Map.hpp
+++ Map.hpp
@@ -64,11 +64,15 @@ public:
   // copy, assign, and destroy Maps.
 
   // EFFECTS : Returns whether this Map is empty.
-  bool empty() const;
+  bool empty() const{
+    return entries.empty();
+  }
 
   // EFFECTS : Returns the number of elements in this Map.
   // NOTE : size_t is an integral type from the STL
-  size_t size() const;
+  size_t size() const{
+    return entries.size();
+  }
 
   // EFFECTS : Searches this Map for an element with a key equivalent
   //           to k and returns an Iterator to the associated value if found,
@@ -77,7 +81,11 @@ public:
   // HINT: Since Map is implemented using a BinarySearchTree that stores
   //       (key, value) pairs, you'll need to construct a dummy value
   //       using "Value_type()".
-  Iterator find(const Key_type& k) const;
+  Iterator find(const Key_type& k) const{
+    Pair_type dummy(k, Value_type());
+    Iterator it = entries.find(dummy);
+    return it;
+  }
 
   // MODIFIES: this
   // EFFECTS : Returns a reference to the mapped value for the given
@@ -108,10 +116,14 @@ public:
   std::pair<Iterator, bool> insert(const Pair_type &val);
 
   // EFFECTS : Returns an iterator to the first key-value pair in this Map.
-  Iterator begin() const;
+  Iterator begin() const{
+    return entries.begin();
+  }
 
   // EFFECTS : Returns an iterator to "past-the-end".
-  Iterator end() const;
+  Iterator end() const{
+    return entries.end();
+  }
 
 private:
   // Add a BinarySearchTree private member HERE.
